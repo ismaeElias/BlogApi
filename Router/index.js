1e72956dcc6f6fc4fc99281487415f991b11394f
Ismael Elias <51543534+ismaeElias@users.noreply.github.com>
2021-01-15 09:08:12 -0300

    criando bearer personalizado

--- Router/index.js
+++ Router/index.js
@@ -9,12 +9,12 @@ const routes = express.Router();
 
 routes.get('/usuarios', UsuarioController.index);
 routes.post('/usuarios', UsuarioController.store);
-routes.delete('/usuarios/:id',passport.authenticate('bearer', {session : false}),UsuarioController.remove);
+routes.delete('/usuarios/:id', auth.bearer ,UsuarioController.remove);
 
 routes.get('/usuarios/postagem', PostagemController.index);
-routes.post('/usuarios/:usuario_id/postagem', passport.authenticate('bearer', {session : false}),PostagemController.store);
+routes.post('/usuarios/:usuario_id/postagem', auth.bearer,PostagemController.store);
 routes.get('/usuarios/:usuario_id/postagem', PostagemController.usuarioPostagem);
-routes.delete('/usuarios/:usuario_id/postagem/:id',passport.authenticate('bearer', {session : false}), PostagemController.remove);
+routes.delete('/usuarios/:usuario_id/postagem/:id',auth.bearer, PostagemController.remove);
 
 routes.post('/login',auth.local, UsuarioController.login);
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -7,13 +7,39 @@ module.exports = {
             { session : false},
             (error, usuario, info) =>  {
                 if(error){
-                    return res.status(500).json({error : error.message})
+                    return res.status(401).json({error : error.message})
                 }
                 if(!usuario){
                     return res.status(401).json();
                 }
-
+                
                 req.user = usuario;
+                return next();
+            }
+        )(req, res, next);
+    },
+
+    bearer : (req, res, next) => {
+        passport.authenticate(
+            'bearer',
+            {session : false},
+            (error, usuario , info) => {
+                
+                if(error && error.name === 'JsonWebTokenError') {
+                    return res.status(401).json({error : error.message});
+                }
+                if(error && error.name === 'TokenExpiredError'){
+                    return res.status(401).json({error : error.message, expiradoEm : error.expiredAt});
+                }
+                if(error){
+                    return res.status(500).json({error : error.message});
+                }
+
+                if(!usuario){
+                    return res.status(401).json();
+                }
+
+                req.user = usuario
                 return next();
             }
         )(req, res, next);
